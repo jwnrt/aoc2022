271406f86903a422095825e3866a8a9910fad0fd
James Wainwright <james.wainwright@lowrisc.org>
2022-12-01 18:20:35 +0000

    Add solution to day1 bonus

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -19,18 +19,36 @@ fn main() -> ! {
     let lines = input_str.split('\n');
 
     let mut accumulator = 0;
-    let mut largest = 0;
+    let mut largest = [0; 3];
 
     for line in lines {
         if line.len() == 0 {
-            largest = cmp::max(largest, accumulator);
+            largest[0] = cmp::max(largest[0], accumulator);
+            sort(&mut largest);
             accumulator = 0;
         } else {
             accumulator += u32::from_str_radix(line, 10).unwrap();
         }
     }
 
-    writeln!(uart, "most calories: {largest}").unwrap();
+    writeln!(uart, "Most calorific elves:").unwrap();
+    for (i, calories) in largest.iter().rev().enumerate() {
+        writeln!(uart, "{}: {calories}", i + 1).unwrap();
+    }
+    writeln!(uart, "total: {}", largest[0] + largest[1] + largest[2]).unwrap();
 
     loop {}
+}
+
+/// Hard coded sort for a three element set.
+fn sort(set: &mut [u32; 3]) {
+    if set[0] > set[1] {
+        set.swap(0, 1);
+    }
+    if set[1] > set[2] {
+        set.swap(1, 2);
+    }
+    if set[0] > set[1] {
+        set.swap(0, 1);
+    }
 }
