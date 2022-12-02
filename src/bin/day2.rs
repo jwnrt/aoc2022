c3a6710facbacfde456cf2ad8039e0727a51c184
James Wainwright <james.wainwright@lowrisc.org>
2022-12-02 09:21:03 +0000

    Add solution to bonus problem

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -18,26 +18,52 @@ fn main() -> ! {
 
     let lines = input_str.split('\n');
 
-    let mut score = 0;
+    let mut score_rps = 0; // Score using the rock/paper/scissors strategy
+    let mut score_ldw = 0; // Score using the lose/draw/win strategy
 
     for line in lines {
-        let points = match line {
-            "A X" => 1 + 3,
-            "A Y" => 2 + 6,
-            "A Z" => 3 + 0,
-            "B X" => 1 + 0,
-            "B Y" => 2 + 3,
-            "B Z" => 3 + 6,
-            "C X" => 1 + 6,
-            "C Y" => 2 + 0,
-            "C Z" => 3 + 3,
-            _ => 0,
-        };
-
-        score += points;
+        score_rps += rps_points(line);
+        score_ldw += ldw_points(line);
     }
 
-    writeln!(uart, "Points: {score}").unwrap();
+    writeln!(
+        uart,
+        "Points with rock/paper/scissors strategy: {score_rps}"
+    )
+    .unwrap();
+    writeln!(uart, "Points with lose/draw/win strategy: {score_ldw}").unwrap();
 
     loop {}
+}
+
+/// Scoring system for the rock/paper/scissors interpretation of codes
+fn rps_points(code: &str) -> u32 {
+    match code {
+        "A X" => 1 + 3,
+        "A Y" => 2 + 6,
+        "A Z" => 3 + 0,
+        "B X" => 1 + 0,
+        "B Y" => 2 + 3,
+        "B Z" => 3 + 6,
+        "C X" => 1 + 6,
+        "C Y" => 2 + 0,
+        "C Z" => 3 + 3,
+        _ => 0,
+    }
+}
+
+/// Scoring system for the lose/draw/win interpretation of codes
+fn ldw_points(code: &str) -> u32 {
+    match code {
+        "A X" => 3 + 0,
+        "A Y" => 1 + 3,
+        "A Z" => 2 + 6,
+        "B X" => 1 + 0,
+        "B Y" => 2 + 3,
+        "B Z" => 3 + 6,
+        "C X" => 2 + 0,
+        "C Y" => 3 + 3,
+        "C Z" => 1 + 6,
+        _ => 0,
+    }
 }
