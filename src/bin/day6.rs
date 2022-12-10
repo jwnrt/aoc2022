42d4526dd1f5d1b164b7f93ff068fbc122de8965
James Wainwright <james.wainwright@lowrisc.org>
2022-12-10 22:07:46 +0000

    Add solution to bonus of day 6
    Switches the uniqueness test from a hardcoded comparison of four bytes to a
    mask that gets XORed with each char.

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -12,19 +12,41 @@ fn main() -> ! {
 
     let input_str = include_str!("data/day6.txt");
 
-    let bytes = input_str.as_bytes();
-    for (i, window) in bytes.windows(4).enumerate() {
-        let dupe = window[0] == window[1]
-            || window[0] == window[2]
-            || window[0] == window[3]
-            || window[1] == window[2]
-            || window[1] == window[3]
-            || window[2] == window[3];
-        if !dupe {
-            writeln!(uart, "{}", i + 4).unwrap();
-            break;
+    let start_of_packet = unique_run(input_str, 4).unwrap();
+    let start_of_message = unique_run(input_str, 14).unwrap();
+
+    writeln!(uart, "Start of packet: {start_of_packet}").unwrap();
+    writeln!(uart, "Start of message: {start_of_message}").unwrap();
+
+    loop {}
+}
+
+// Only works for [a-z] currently
+fn unique_run(input: &str, len: usize) -> Option<usize> {
+    let mut mask: u32 = 0;
+    let mut leaving_byte: u8 = 0;
+
+    for (i, window) in input.as_bytes().windows(len).enumerate() {
+        if i == 0 {
+            window.iter().for_each(|b| mask ^= 1 << value(*b));
+        } else {
+            mask ^= 1 << value(leaving_byte);
+            mask ^= 1 << value(*window.last().unwrap());
+        }
+
+        leaving_byte = *window.first().unwrap();
+
+        if mask.count_ones() as usize == len {
+            return Some(i + len);
         }
     }
 
-    loop {}
+    None
+}
+
+fn value(b: u8) -> u32 {
+    match b as char {
+        'a'..='z' => b as u32 - 96,
+        _ => 0,
+    }
 }
